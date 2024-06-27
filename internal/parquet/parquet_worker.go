a4a1c914e8253f82696dfe8c7237654617eba2f5
kai <kai@turbot.com>
2024-06-27 13:07:33 +0100

    Fix parquet generation - use SQL to do partitioning

--- internal/parquet/parquet_worker.go
+++ internal/parquet/parquet_worker.go
@@ -3,11 +3,11 @@ package parquet
 import (
 	"errors"
 	"fmt"
-	"github.com/google/uuid"
 	"github.com/turbot/tailpipe-plugin-sdk/plugin"
 	"log/slog"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 // parquetConversionWorker is an implementation of worker that converts JSONL files to Parquet
@@ -44,16 +44,12 @@ func (w *parquetConversionWorker) doJSONToParquetConversion(job fileJob) error {
 	jsonFileName := plugin.ExecutionIdToFileName(job.groupId, job.chunkNumber)
 	jsonFilePath := filepath.Join(w.sourceDir, jsonFileName)
 
-	//slog.Debug("source file", "path", jsonFilePath)
-
 	// process the jobGroup
 	err := w.convertFile(jsonFilePath, job.collectionType)
 	if err != nil {
 		slog.Error("failed to convert file", "error", err)
 		return fmt.Errorf("failed to convert file %s: %w", jsonFilePath, err)
 	}
-
-	//slog.Debug("converted file successfully, deleting JSONL file", "path", jsonFilePath)
 
 	// delete JSON file (configurable?)
 	if err := os.Remove(jsonFilePath); err != nil {
@@ -85,91 +81,31 @@ func (w *parquetConversionWorker) convertFile(jsonlFilePath, collectionType stri
 	}
 	defer func() {
 		// drop the view
-		if _, dropErr := w.db.Exec("DROP VIEW json_view;"); dropErr != nil {
+		if _, dropErr := w.db.Exec("DROP VIEW IF EXISTS json_view;"); dropErr != nil {
 			err = errors.Join(err, fmt.Errorf("failed to drop view: %w", dropErr))
 		}
 	}()
 
-	// we need to generate a different parquet file for each combination of the specified columns:
-	// tp_collection, tp_connection, tp_year, tp_month, tp_day
-	// Get distinct combinations
-	query := `
-		SELECT DISTINCT 
-			tp_collection, 
-			tp_connection, 
-			tp_year, 
-			tp_month, 
-			tp_day 
-		FROM json_view
-	`
-	rows, err := w.db.Query(query)
+	// determine the root based on the collection type
+	fileRoot := w.getParquetFileRoot(collectionType)
+	if err := os.MkdirAll(filepath.Dir(fileRoot), 0755); err != nil {
+		return fmt.Errorf("failed to create parquet folder: %w", err)
+	}
+
+	// Create a query to write to partitioned parquet files
+	partitionColumns := []string{"tp_collection", "tp_connection", "tp_year", "tp_month", "tp_day"}
+	exportQuery := fmt.Sprintf(`COPY  json_view TO '%s' (FORMAT PARQUET, PARTITION_BY (%s), OVERWRITE_OR_IGNORE, FILENAME_PATTERN "file_{uuid}");`, fileRoot, strings.Join(partitionColumns, ","))
+	_, err = w.db.Exec(exportQuery)
 	if err != nil {
-		return fmt.Errorf("failed to query distinct combinations: %w", err)
-	}
-	defer rows.Close()
-
-	var errList []error
-	// Iterate over each combination and export the data for that combination
-	for rows.Next() {
-		var (
-			tpCollection string
-			tpConnection string
-			tpYear       string
-			tpMonth      string
-			tpDay        string
-		)
-		if err := rows.Scan(&tpCollection, &tpConnection, &tpYear, &tpMonth, &tpDay); err != nil {
-			return fmt.Errorf("failed to scan row: %w", err)
-		}
-
-		// Generate the parquet file path name based on the naming convention
-		filePath := w.getParquetFilePath(collectionType, tpCollection, tpConnection, tpYear, tpMonth, tpDay)
-		// ensure the folder exists
-		if err := os.MkdirAll(filepath.Dir(filePath), 0755); err != nil {
-			errList = append(errList, fmt.Errorf("failed to create directory %s : %w", filepath.Dir(filePath), err))
-			continue
-		}
-
-		// Create a query to select data for the specific combination
-		exportQuery := fmt.Sprintf(`
-			COPY (
-				WITH data_group AS (
-					SELECT * FROM json_view 
-					WHERE tp_collection = '%s'
-					AND tp_connection = '%s'
-					AND tp_year = '%s'
-					AND tp_month = '%s'
-					AND tp_day = '%s'
-				)
-				SELECT * FROM data_group
-			) TO '%s' (FORMAT PARQUET);
-		`, tpCollection, tpConnection, tpYear, tpMonth, tpDay, filePath)
-
-		_, err = w.db.Exec(exportQuery)
-		if err != nil {
-			errList = append(errList, fmt.Errorf("failed select JSON data: %w", err))
-			continue
-		}
-
-		//slog.Debug("exported data to parquet", "file", filePath)
+		return fmt.Errorf("failed to export data to parquet: %w", err)
 	}
 
-	if err := rows.Err(); err != nil {
-		errList = append(errList, fmt.Errorf("error converting JSONL file to Parquet: %w", err))
-	}
-	if len(errList) > 0 {
-		return errors.Join(errList...)
-	}
+	//slog.Debug("exported data to parquet", "file", filePath)
 
 	return nil
 }
 
-// getParquetFilePath generates the file path for the parquet file based on the naming convention
-func (w *fileWorkerBase) getParquetFilePath(collectionType string, tpCollection string, tpConnection string, tpYear string, tpMonth string, tpDay string) string {
-	// generate uuid for the filename
-	id := uuid.New().String()
-	filePath := fmt.Sprintf("%s/tp_collection=%s/tp_connection=%s/tp_year=%s/tp_month=%s/tp_day=%s/%s.parquet",
-		collectionType, tpCollection, tpConnection, tpYear, tpMonth, tpDay, id)
-	return filepath.Join(w.destDir, filePath)
-
+// getParquetFileRoot generates the file root for the parquet file based on the naming convention
+func (w *fileWorkerBase) getParquetFileRoot(collectionType string) string {
+	return filepath.Join(w.destDir, collectionType)
 }
