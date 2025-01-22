13d7bd1d61b332db668cd079c15f1ea63febf2b2
kai <kai@turbot.com>
2025-01-22 00:04:14 +0000

    update min core plugin version

--- internal/constants/plugin.go
+++ internal/constants/plugin.go
@@ -3,6 +3,6 @@ package constants
 const (
 	CorePluginName       = "core"
 	CorePluginFullName   = "hub.tailpipe.io/plugins/turbot/core@latest"
-	MinCorePluginVersion = "v0.0.1-rc.6"
+	MinCorePluginVersion = "v0.0.1-rc.5"
 	TailpipeHubOCIBase   = "hub.tailpipe.io/"
 )
