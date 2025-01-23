c0c6a7ce51f0e846ed83b82ae1cf52de7f5d32ba
kai <kai@turbot.com>
2025-01-23 15:42:57 +0000

    remove Timing and TimingCollection and all time tracking linting and tidying

--- internal/constants/plugin.go
+++ internal/constants/plugin.go
@@ -3,6 +3,6 @@ package constants
 const (
 	CorePluginName       = "core"
 	CorePluginFullName   = "hub.tailpipe.io/plugins/turbot/core@latest"
-	MinCorePluginVersion = "v0.1.0-rc.1"
+	MinCorePluginVersion = "v0.1.0-rc.2"
 	TailpipeHubOCIBase   = "hub.tailpipe.io/"
 )
