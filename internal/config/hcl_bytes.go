b8c98eb3f9bfc8b7173b91a25a71b8108e5243d4
kai <kai@turbot.com>
2025-02-07 12:10:08 +0000

    Add tactical fix to handle EOT in HclBytesForRange

--- internal/config/hcl_bytes.go
+++ internal/config/hcl_bytes.go
@@ -3,6 +3,7 @@ package config
 import (
 	"github.com/hashicorp/hcl/v2"
 	"github.com/turbot/pipe-fittings/v2/hclhelpers"
+	"strings"
 )
 
 type HclBytes struct {
@@ -10,9 +11,17 @@ type HclBytes struct {
 	Range hclhelpers.Range `cty:"range"`
 }
 
+// HclBytesForRange extracts the HCL bytes for a given range from a source HCL byte buffer
+// This is used to extract the HCL for a unknown attributes so we can pass it to a plugin to parse
 func HclBytesForRange(sourceHcl []byte, r hcl.Range) *HclBytes {
 	//  get the bytes for the attribute (note: clone the byte buffer)
 	hclForRange := append([]byte{}, sourceHcl[r.Start.Byte:r.End.Byte]...)
+
+	// TACTICAL: if the HCL ends with "EOT" we need to add a newline to ensure the HCL parser doesn't fail
+	// with the error "Unterminated template string; No closing marker was found for the string."
+	if strings.HasSuffix(string(hclForRange), "EOT") {
+		hclForRange = append(hclForRange, '\n')
+	}
 	return &HclBytes{
 		Hcl:   hclForRange,
 		Range: hclhelpers.NewRange(r),
