820ae5867a8037e9765591d0dc90bf3ae44151e2
kai <kai@turbot.com>
2025-03-03 11:21:25 +0000

    ConfigParseContext AddResource now uses GetResourceCtyValue so we populate nested struct props Add CtyValue to all resources so we can use GetResourceCtyValue delete plugin resource Add FormatMap to PluginDescribeResponse

--- internal/plugin/describe_response.go
+++ internal/plugin/describe_response.go
@@ -1,6 +1,7 @@
 package plugin
 
 import (
+	"github.com/turbot/tailpipe-plugin-sdk/formats"
 	"github.com/turbot/tailpipe-plugin-sdk/grpc/proto"
 	"github.com/turbot/tailpipe-plugin-sdk/row_source"
 	"github.com/turbot/tailpipe-plugin-sdk/schema"
@@ -10,11 +11,13 @@ type PluginDescribeResponse struct {
 	Name         string                       `json:"name"`
 	TableSchemas schema.SchemaMap             `json:"tables"`
 	Sources      row_source.SourceMetadataMap `json:"sources"`
+	Formats	  formats.FormatMap            `json:"formats"`
 }
 
 func DescribeResponseFromProto(resp *proto.DescribeResponse) *PluginDescribeResponse {
 	return &PluginDescribeResponse{
 		TableSchemas: schema.SchemaMapFromProto(resp.Schemas),
 		Sources:      row_source.SourceMetadataMapFromProto(resp.Sources),
+		Formats:      formats.FormatMapFromProto(resp.Formats),
 	}
 }
