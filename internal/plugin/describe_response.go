88c2c736352e043e8b568f5fa221dd3c13733fcb
kai <kai@turbot.com>
2025-03-03 18:59:34 +0000

    null checking in DescribeResponseFromProto

--- internal/plugin/describe_response.go
+++ internal/plugin/describe_response.go
@@ -15,9 +15,18 @@ type PluginDescribeResponse struct {
 }
 
 func DescribeResponseFromProto(resp *proto.DescribeResponse) *PluginDescribeResponse {
-	return &PluginDescribeResponse{
-		TableSchemas: schema.SchemaMapFromProto(resp.Schemas),
-		Sources:      row_source.SourceMetadataMapFromProto(resp.Sources),
-		Formats:      formats.FormatMapFromProto(resp.Formats),
+	res := &PluginDescribeResponse{}
+	if resp == nil {
+		return res
 	}
+	if resp.Schemas != nil {
+		res.TableSchemas = schema.SchemaMapFromProto(resp.Schemas)
+	}
+	if resp.Sources != nil {
+		res.Sources = row_source.SourceMetadataMapFromProto(resp.Sources)
+	}
+	if resp.Formats != nil {
+		res.Formats = formats.FormatMapFromProto(resp.Formats)
+	}
+	return res
 }
