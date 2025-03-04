f01738d2eea37e2a830c817a5bd4dc6a14c12f91
kaidaguerre <kai@turbot.com>
2025-03-04 12:43:29 +0000

    DeleteParquetFiles only prunes the partition folder. Closes #227 (#228)
    * DeleteParquetFiles only prunes the partition folder
    * PruneTree handles non target existent folder
    * fix multi-partition error handling - collect all partitions if one fails
    * update PartitionResource.setFileInformation to use filepaths.GetParquetPartitionPath
    * linting

--- internal/filepaths/parquet.go
+++ internal/filepaths/parquet.go
@@ -2,6 +2,7 @@ package filepaths
 
 import (
 	"fmt"
+
 	"path/filepath"
 )
 
@@ -13,6 +14,6 @@ func GetParquetFileGlobForPartition(dataDir, tableName, partitionName, fileRoot 
 	return filepath.Join(dataDir, fmt.Sprintf("tp_table=%s/tp_partition=%s/*/*/%s*.parquet", tableName, partitionName, fileRoot))
 }
 
-func GetParquetPartitionPath(dataDir, tableName, partitionName, fileRoot string) string {
+func GetParquetPartitionPath(dataDir, tableName, partitionName string) string {
 	return filepath.Join(dataDir, fmt.Sprintf("tp_table=%s/tp_partition=%s", tableName, partitionName))
 }
