ee378297efd3d1341861157f4eb5a002c361b88a
kai <kai@turbot.com>
2025-03-19 16:08:25 +0000

    update required core version to v0.1.4-rc.0

--- internal/constants/plugin.go
+++ internal/constants/plugin.go
@@ -3,7 +3,7 @@ package constants
 const (
 	CorePluginName       = "core"
 	CorePluginFullName   = "hub.tailpipe.io/plugins/turbot/core@latest"
-	MinCorePluginVersion = "v0.1.3"
+	MinCorePluginVersion = "v0.1.4-rc.0"
 	// TailpipeHubOCIBase is the tailpipe hub URL
 	TailpipeHubOCIBase = "hub.tailpipe.io/"
 	// BaseImageRef is the prefix for all tailpipe plugin images
