8c0371732546ac71ce0ab74b7d1ee7760fd97c1e
kai <kai@turbot.com>
2025-04-10 13:17:38 +0100

    remove time_format from column def

--- internal/config/column.go
+++ internal/config/column.go
@@ -18,8 +18,6 @@ type Column struct {
 	Required *bool `hcl:"required" cty:"required"`
 	// An optional null value for the column. If the source value contains this, the column will be null
 	NullIf *string `hcl:"null_if" cty:"null_if"`
-	// The strptime format of the time field so it can be recognized and analyzed properly.
-	TimeFormat *string `hcl:"time_format" cty:"time_format"`
 	// A duck DB transform function to apply to the column. This should be expressed as a SQL function
 	// If a Transform is provided, no source should be provided.
 	// e.g. "upper(name)"
@@ -35,7 +33,6 @@ func (c Column) ToProto() *proto.ColumnSchema {
 		Description: typehelpers.SafeString(c.Description),
 		NullValue:   typehelpers.SafeString(c.NullIf),
 		Required:    typehelpers.BoolValue(c.Required),
-		TimeFormat:  typehelpers.SafeString(c.TimeFormat),
 		Transform:   typehelpers.SafeString(c.Transform),
 	}
 	// override the source name if it is set
