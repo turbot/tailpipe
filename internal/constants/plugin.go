9697d8552edc7e535623a55848ec584588f2ef31
Puskar Basu <45908484+pskrbasu@users.noreply.github.com>
2025-06-04 18:28:11 +0530

    Update Core plugin version to v0.2.6

--- internal/constants/plugin.go
+++ internal/constants/plugin.go
@@ -3,7 +3,7 @@ package constants
 const (
 	CorePluginName       = "core"
 	CorePluginFullName   = "hub.tailpipe.io/plugins/turbot/core@latest"
-	MinCorePluginVersion = "v0.2.5"
+	MinCorePluginVersion = "v0.2.6"
 	// TailpipeHubOCIBase is the tailpipe hub URL
 	TailpipeHubOCIBase = "hub.tailpipe.io/"
 	// BaseImageRef is the prefix for all tailpipe plugin images
